15c9d3a6d28187dc56295e28d7d3022f57d52775
Edgar Friendly <thelema314@gmail.com>
2022-09-11 05:44:55 +0000

    Move assets back to _assets, configure webpack to process properly

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,17 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 process.env.NODE_ENV = Encore.isProduction() ? 'production' : 'dev';
 
 Encore
-//  .setOutputPath('assets/')
-//  .setPublicPath('/assets')
+  .setOutputPath('assets/')
+  .setPublicPath('/assets')
 //  .addStyleEntry('css/app', './_assets/css/app.css')
 //  .addEntry('js/app', './_assets/js/app.js')
+  .addStyleEntry('css/beautifuljekyll-minimal', './_site/assets/css/beautifuljekyll-minimal.css')
+  .addStyleEntry('css/beautifuljekyll', './_site/assets/css/beautifuljekyll.css')
+  .addStyleEntry('css/bootstrap-social', './_site/assets/css/bootstrap-social.css')
+  .addStyleEntry('css/pygment_highlights', './_site/assets/css/pygment_highlights.css')
+  .addStyleEntry('css/staticman', './_site/assets/css/staticman.css')
+  .addEntry('js/beautifuljekyll', './_site/assets/js/beautifuljekyll.js')
+  .addEntry('js/staticman', './_site/assets/js/staticman.js')
   .enablePostCssLoader()
   .disableSingleRuntimeChunk()
   .enableSourceMaps(!Encore.isProduction());
